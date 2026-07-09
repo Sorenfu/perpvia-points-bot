fc89a5f94b3d9b2a6fdf864dce8ccc5417d7e7be
Sorenfu <fujinru01@gmail.com>
2026-07-09 14:44:22 +0800

    Delete discord directory

--- discord/sync.py
+++
@@ -1,9 +0,0 @@
-import os, discord
-async def sync_debug(bot):
-    guild=discord.Object(id=int(os.getenv('GUILD_ID')))
-    print('Clearing global commands')
-    bot.tree.clear_commands(guild=None)
-    await bot.tree.sync()
-    print('Sync guild commands')
-    await bot.tree.sync(guild=guild)
-    print('Sync completed')
