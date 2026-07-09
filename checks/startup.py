9f02317b697028778f2cdf9d29f30f4eea7feebc
Sorenfu <fujinru01@gmail.com>
2026-07-09 14:44:13 +0800

    Delete checks directory

--- checks/startup.py
+++
@@ -1,7 +0,0 @@
-import os
-
-def validate_env():
-    for key in ['DISCORD_TOKEN','GUILD_ID']:
-        if not os.getenv(key):
-            raise RuntimeError('Missing '+key)
-    print('Environment OK')
